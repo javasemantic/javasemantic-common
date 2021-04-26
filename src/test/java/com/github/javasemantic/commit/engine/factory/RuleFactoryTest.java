612b744e4a5192650b547417e2ae023aa99e99c8
Coenraad Human <coen.human@gmail.com>
2021-04-26 12:13:27 +0200

    fix: rule factory test fixed

--- src/test/java/com/github/javasemantic/commit/engine/factory/RuleFactoryTest.java
+++ src/test/java/com/github/javasemantic/commit/engine/factory/RuleFactoryTest.java
@@ -1,6 +1,5 @@
 package com.github.javasemantic.commit.engine.factory;
 
-import com.github.javasemantic.commit.engine.factory.enums.CommitPartRuleEnum;
 import com.github.javasemantic.commit.engine.factory.enums.ConventionalRuleEnum;
 import com.github.javasemantic.commit.engine.factory.enums.StructuralRuleEnum;
 
@@ -13,17 +12,6 @@ import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 @ExtendWith(MockitoExtension.class)
 public class RuleFactoryTest {
-
-    @Test
-    public void when_getNewCommitRule_should_returnNewOneEachTime() {
-        var ruleOne = RuleFactory.get(CommitPartRuleEnum.TYPE_RULE);
-        var ruleTwo = RuleFactory.get(CommitPartRuleEnum.TYPE_RULE);
-
-        System.out.println(ruleOne);
-        System.out.println(ruleTwo);
-
-        assertNotEquals(ruleOne.toString(), ruleTwo.toString());
-    }
 
     @Test
     public void when_getNewStructuralRule_should_returnNewOneEachTime() {
